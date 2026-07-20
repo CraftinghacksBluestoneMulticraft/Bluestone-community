6a41b7ea3d70867a0816ad961ab5c8ca2df2b44b
Bluestone Community <craftinghacksmulticraft@gmail.com>
2026-07-20 16:11:31 +0200

    update

---
+++ script.js
@@ -0,0 +1,11 @@
+function blurSite() {
+    const htmlElement = `
+    <div id="cover" style="display:flex;position:fixed;top:0;left:0;backdrop-filter:blur(10px);-webkit-backdrop-filter:blur(10px);background:rgba(255, 255, 255, 0.2);width:100%;height:100%;text-align:center;">
+        <h1 style="font-size:60px;place-self: center;">Website is in progress!</h1>
+    </div>
+    `;
+    document.querySelector("body").innerHTML += htmlElement;
+    return true;
+}
+
+blurSite();
